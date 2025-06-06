63f6e26e6319504fc890708757472f7f7a3771a6
TheEmeraldDerpLeader <48189298+TheEmeraldDerpLeader@users.noreply.github.com>
2025-06-06 00:00:26 -0400

    First commit
    Planning and design document

---
+++ Source/Program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//To Do:
+//Setup build config for SFML
+//Basic SFML window
+//Test SFML audio output
+//Windows API boilerplate
+//Test the new API function (ActivateAudioInterfaceAsync)
+int main()
+{
+	std::cout << "Test!\n";
+
+	return 0;
+}
