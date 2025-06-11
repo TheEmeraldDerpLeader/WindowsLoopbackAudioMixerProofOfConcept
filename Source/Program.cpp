e88f4632e8720c57b105c2668bd0fc13d4f1b48c
TheEmeraldDerpLeader <48189298+TheEmeraldDerpLeader@users.noreply.github.com>
2025-06-11 17:38:39 -0400

    Continued testing Windows COM API
    Still haven't started on the ActivateAudioInterfaceAsync yet. Mostly just reviewing how COM works and copying bits from a different project I made where I tested outputting audio.

--- Source/Program.cpp
+++ Source/Program.cpp
@@ -6,6 +6,8 @@
 
 #include <Windows.h>
 #include <Psapi.h>
+#include <mmdeviceapi.h>
+#include <Audioclient.h>
 
 //To Do:
 
@@ -50,6 +52,7 @@ int main()
 
 void WindowsTest()
 {
+    //basic Windows API test
     DWORD processes[2048];
     DWORD retSize;
     EnumProcesses(processes, sizeof(DWORD)*2048, &retSize);
@@ -58,10 +61,53 @@ void WindowsTest()
         HANDLE process = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processes[i]);
         if (process != NULL)
         {
-            wchar_t processName[2048];
-            GetModuleBaseName(process, NULL, processName, 2048); //this errors
-            //std::wcout << std::wstring(processName) << '\n';
+            char processName[2048];
+            DWORD nameSize = GetProcessImageFileNameA(process, processName, 2048);
+            std::string pName;
+            
+            //just get executable name
+            int index = 0;
+            for (int i = nameSize-1; i >= 0; i--)
+                if (processName[i] == '\\')
+                {
+                    index = i+1;
+                    break;
+                }
+            for (int i = index; i < nameSize; i++)
+                pName.push_back(processName[i]);
+            
+            //std::cout << pName << '\n';
             CloseHandle(process);
         }
     }
+
+    //Get audio devices
+    //use cocreateinstance to get an audio device enumerator
+    HRESULT hr;
+    IMMDeviceEnumerator *pEnumerator = NULL;
+    IMMDevice *pDevice = NULL;
+    IAudioClient *pAudioClient = NULL;
+    int count = 0;
+
+    const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+    const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+    const IID IID_IAudioClient = __uuidof(IAudioClient);
+
+    //for testing purposes just doing COM stuff here
+    CoInitializeEx(0,COINIT::COINIT_APARTMENTTHREADED);
+
+    //create a device enumerator object
+    CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL, IID_IMMDeviceEnumerator,(void**)&pEnumerator);
+
+    pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+    pDevice->Activate(IID_IAudioClient, CLSCTX_ALL,NULL, (void**)&pAudioClient);
+    
+    if (pEnumerator != NULL) { pEnumerator->Release(); pEnumerator = NULL; }
+    if (pDevice != NULL) { pDevice->Release(); pDevice = NULL; }
+    if (pAudioClient != NULL) { pAudioClient->Release(); pAudioClient = NULL; }
+    CoUninitialize();
+
+
+
+    //HRESULT err = ActivateAudioInterfaceAsync();
 }
